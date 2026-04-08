1948858a4379ac84780cd8d6d0f5ccfc4aa83815
Emanuele Vaccaro <evaccaro.001@gmail.com>
2026-04-08 07:42:28 +0200

    add url api and token

---
+++ assets/js/index-script.js
@@ -0,0 +1,3 @@
+// TODO: Strive School Token API : eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OWQ1NDM3NGJhMGYxMjAwMTUyZTc3NmIiLCJpYXQiOjE3NzU1ODQxMTYsImV4cCI6MTc3Njc5MzcxNn0.IJzM3lyFfQC9wLFlzv2bE1mbyIVPGWi7N4shdrUZE5w
+// TODO: Link to fetch API:  https://striveschool-api.herokuapp.com/api/product/
+
